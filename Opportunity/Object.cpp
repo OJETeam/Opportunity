b3ccf793e1051dd6f9334130ad5915939bb8eae5
Eugen1344 <eugen1344@gmail.com>
2019-12-02 16:19:31 +0200

    *

--- Opportunity/Object.cpp
+++ Opportunity/Object.cpp
@@ -30,11 +30,8 @@ void Object::UpdateModel()
 
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vector2), nullptr);
 	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vector2) * model.model.size(), model.model.data(), GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
